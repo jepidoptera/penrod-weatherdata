f306298261f194cb82380ed123ff77faa0632709
jepidoptera <jepidoptera@live.com>
2019-09-15 22:15:34 -0500

    render handlebars templates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 // load API key from .env file
 const apiKey = process.env.APIKEY || "no_api_key";
 
@@ -12,19 +12,69 @@ console.log(apiKey);
 // Serve static files
 app.use(express.static(__dirname + '/public'));
 
+// use handlebars
+var exphbs = require("express-handlebars");
+
+// capitalize words on demand
+app.engine(".hbs", exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+    helpers: {
+        'Capitalize': function(string)
+        {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+    }
+}));
+
+app.set("view engine", ".hbs");
+
 // Serve app
 console.log('Listening on: http://localhost:' + port);
 
-app.get('/weather/:city', (req, res) => {
+app.get('/', (req, res) => {
+    // home page
+    let cities = [
+        {name: "Chicago", id: 4887398}, 
+        {name: "Minneapolis", id: 5037649}, 
+        {name: "Milwaukee", id: 5263045}, 
+        {name: "Dallas", id: 4684888}
+    ]
+    console.log('rendering city select page...');
+    res.render("index", {cities: cities});
+})
+
+app.get('/weather/:cityId', (req, res) => {
     // load data for selected city
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${apiKey}`)
+    let urlString = !isNaN(req.params.cityId)
+        ? `http://api.openweathermap.org/data/2.5/weather?id=${req.params.cityId}&appid=${apiKey}`
+        // or, in case you manually enter a city name into the url...
+        : `http://api.openweathermap.org/data/2.5/weather?q=${req.params.cityId}&appid=${apiKey}`
+    axios.get(urlString)
     // axios.get('https://gogobackend.azurewebsites.net/api/games/all')
     .then(function (response) {
-        // handle success
-        console.log(response);
-        // render loading page
-        // res.render("weather");      
-        res.send(response.data)
+        // success
+        // parsing out data for the template
+        let weatherData = {
+            city: response.data.name,
+            country: response.data.sys.country,
+            description: response.data.weather[0].description,
+            // this is in degrees Kelvin, btw. page will want to convert
+            temperature: response.data.main.temp,
+            pressure: response.data.main.pressure,
+            humidity: response.data.main.humidity,
+            sunrise: response.data.sys.sunrise,
+            sunset: response.data.sys.sunset,
+            timezone: response.data.timezone
+        }
+        // see what we've got...
+        console.log(weatherData);
+        // render weather page
+        // but wait a sec so we can look at the loading icon
+        // setTimeout(() => {
+            res.render("weather", weatherData);      
+        // }, 1000);
+        // res.send(response.data)
     })
     .catch(function (error) {
         // handle error
